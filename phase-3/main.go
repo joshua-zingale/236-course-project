7bd0821a84ed0ed4ca0bf300df2cb492616e41f6
Joshua Zingale <jtzingale@gmail.com>
2025-11-25 20:26:26 -0800

    Added cmd options for db credentials

--- phase-3/main.go
+++ phase-3/main.go
@@ -13,11 +13,14 @@ import (
 
 func main() {
 
+	dbUser := flag.String("dbuser", "postgres", "the database user")
+	dbPassword := flag.String("dbpassword", "postgres", "the database user's password")
+	dbName := flag.String("dbname", "postgres", "the database name")
 	dbAddress := flag.String("dbaddress", "127.0.0.1", "the address, e.g. 127.0.0.1, of the postgres database.")
 	dbPort := flag.Int("dbport", 5432, "the port of the postgres database")
 	flag.Parse()
 
-	dbpool, err := pgxpool.New(context.Background(), fmt.Sprintf("postgres://postgres:postgres@%s:%d/postgres", *dbAddress, *dbPort))
+	dbpool, err := pgxpool.New(context.Background(), fmt.Sprintf("postgres://%s:%s@%s:%d/%s", *dbUser, *dbPassword, *dbAddress, *dbPort, *dbName))
 	if err != nil {
 		log.Fatalf("Could not connect to database: %s", err)
 	}
